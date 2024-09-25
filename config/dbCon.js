8922caea8035fcc24765dbb4403068e6dc914b6b
vykis <vykintaspilipauskas@gmail.com>
2024-09-25 21:47:04 +0300

    :updated dbcon file

--- config/dbCon.js
+++ config/dbCon.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URI, {});
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
   } catch (error) {
     console.log(error);
   }
